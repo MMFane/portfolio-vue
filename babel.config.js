0c04ae837e9914ad40b140419513d4c8327f7ec2
Sam <sam.williams@littlebytegames.com>
2020-08-16 15:03:40 -0700

    remove Google Fonts plugin from Babel config

--- babel.config.js
+++ babel.config.js
@@ -3,12 +3,5 @@ const GoogleFontsPlugin = require('google-fonts-webpack-plugin')
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  plugins: [
-    new GoogleFontsPlugin({
-      fonts: [
-        { family: 'Nunito' },
-      ],
-    }),
-  ],
+  ]
 }
